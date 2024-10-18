9fd8c71ed52028e5ce652ca3144ffab93b7e8a37
Jairo <jairo.perez.ramon.info@gmail.com>
2024-10-18 18:37:02 +0200

    Working on ActivityMain2

--- app/src/main/java/com/example/mibocata/MainActivity2.kt
+++ app/src/main/java/com/example/mibocata/MainActivity2.kt
@@ -1,28 +1,22 @@
 package com.example.mibocata
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val btnlogin : Button = findViewById(R.id.login)
-        btnlogin.setOnClickListener {
-            var intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
