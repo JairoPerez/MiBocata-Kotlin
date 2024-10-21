c9ec9af02d21580416b67df4431050e8739db115
Jairo <jairo.perez.ramon.info@gmail.com>
2024-10-21 20:29:17 +0200

    Segunda pantalla funcional

--- app/src/main/java/com/example/mibocata/MainActivity.kt
+++ app/src/main/java/com/example/mibocata/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +15,6 @@ class MainActivity : AppCompatActivity() {
         btnlogin.setOnClickListener {
             var intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
--- app/src/main/java/com/example/mibocata/MainActivity2.kt
+++ app/src/main/java/com/example/mibocata/MainActivity2.kt
@@ -1,22 +1,28 @@
 package com.example.mibocata
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
+            val btnFrio : View = findViewById(R.id.ViewFrio)
+            val btnCaliente : View = findViewById(R.id.ViewCaliente)
 
-        }
+            btnFrio.setOnClickListener {
+            var intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+            }
+            btnCaliente.setOnClickListener {
+                var intent = Intent(this, MainActivity3::class.java)
+                startActivity(intent)
+            }
     }
 }
