b5aff923b3555d52d6c92c051993926a9fa62a2d
Jairo <jairo.perez.ramon.info@gmail.com>
2024-10-23 13:41:30 +0200

    Diseño pantallas completado

--- app/src/main/java/com/example/mibocata/MainActivity2.kt
+++ app/src/main/java/com/example/mibocata/MainActivity2.kt
@@ -3,6 +3,7 @@ package com.example.mibocata
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,16 +14,34 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2);
 
-            val btnFrio : View = findViewById(R.id.ViewFrio)
-            val btnCaliente : View = findViewById(R.id.ViewCaliente)
+            val btn_frio : View = findViewById(R.id.view_frio)
+            val btn_caliente : View = findViewById(R.id.view_caliente)
+            val btn_invoice : ImageView = findViewById(R.id.invoice_icon)
+            val btn_profile : ImageView = findViewById(R.id.icon_profile)
+            val btn_calendar : ImageView = findViewById(R.id.icon_calendar)
 
-            btnFrio.setOnClickListener {
-            var intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-            }
-            btnCaliente.setOnClickListener {
+            btn_frio.setOnClickListener {
                 var intent = Intent(this, MainActivity3::class.java)
                 startActivity(intent)
             }
+            btn_caliente.setOnClickListener {
+                var intent = Intent(this, MainActivity3::class.java)
+                startActivity(intent)
+            }
+            btn_invoice.setOnClickListener{
+                var intent = Intent(this, MainActivity3::class.java)
+                startActivity(intent)
+            }
+            btn_profile.setOnClickListener{
+                var intent = Intent(this, MainActivity3::class.java) //CAMBIAR A LA PANTALLA DEL PERFIL MAINACTIVITY5
+                startActivity(intent)
+            }
+            btn_calendar.setOnClickListener{
+                var intent = Intent(this, MainActivity3::class.java)  //CAMBIAR A LA PANTALLA DEL CALENDARIO MAINACTIVITY4
+                startActivity(intent)
+            }
+
+
+
     }
 }
--- app/src/main/java/com/example/mibocata/MainActivity3.kt
+++ app/src/main/java/com/example/mibocata/MainActivity3.kt
@@ -1,6 +1,8 @@
 package com.example.mibocata
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,20 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+
+
+        val btn_invoice : ImageView = findViewById(R.id.invoice_icon)
+        val btn_profile : ImageView = findViewById(R.id.icon_profile)
+        val btn_calendar : ImageView = findViewById(R.id.icon_calendar)
+
+        btn_invoice.setOnClickListener{
+            var intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
         }
+
+
     }
 }
